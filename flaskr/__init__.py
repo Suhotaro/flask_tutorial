6479c74621656cdb8cf86d3b4ce2f8a0c6a3753b
Roma Peresypkin <r.peresypkin@vector.net>
2020-02-17 08:12:59 +0100

    logging configuration

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,29 @@
 import os
 
 from flask import Flask
+from logging.config import dictConfig
 
 
 def create_app(test_config=None):
     # create and configure the app
+
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            # is changeable
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
