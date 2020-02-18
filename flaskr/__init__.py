4405ac78662c5fa7a1ee6cc1fde276f9dede4ff0
Roma Peresypkin <r.peresypkin@vector.net>
2020-02-18 13:08:10 +0100

    signals

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from logging.config import dictConfig
+from flask import signals
 
 
 def create_app(test_config=None):
@@ -51,6 +52,32 @@ def create_app(test_config=None):
     def handle(e):
         return f"zero dibison: ---->> {e} <<----", 500
 
+# signals -------------------------------------------------------------
+    def log_template_renders(sender, template, context, **extra):
+        sender.logger.debug('Rendering template "%s" with context %s',
+                         template.name or 'string template',
+                         context)
+
+    from flask import template_rendered
+    template_rendered.connect(log_template_renders, app, weak=False)
+
+
+    def log_request(sender, **extra):
+        sender.logger.info('Request context is set up')
+
+    from flask import request_started
+    request_started.connect(log_request, app, weak=False)
+
+
+    def log_response(sender, response, **extra):
+        sender.logger.info('Request context is about to close down.  '
+                            'Response: %s', response)
+
+    from flask import request_finished
+    request_finished.connect(log_response, app, weak=False)
+
+# signals -------------------------------------------------------------
+
     from . import db
     db.init_app(app)
 
@@ -60,6 +87,9 @@ def create_app(test_config=None):
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
+
+
+    app.logger.info(f"signal: {signals.signals_available}")
 
     app.logger.info("FLASKR IS INITIALIZED")
 
